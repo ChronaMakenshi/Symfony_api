a9a3dd29c3e26cdb003686c34483b0662b981e98
Mestdagh christophe <77621850+ChronaMakenshi@users.noreply.github.com>
2023-02-09 18:18:22 +0100

    Symfony api

---
+++ src/Controller/AuthorController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Author;
+use App\Repository\AuthorRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use JMS\Serializer\SerializationContext;
+use JMS\Serializer\SerializerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Symfony\Contracts\Cache\ItemInterface;
+use Symfony\Contracts\Cache\TagAwareCacheInterface;
+
+class AuthorController extends AbstractController
+{
+    #[Route('/api/authors', name: 'app_author', methods: ['GET'])]
+    public function getAuthorList(AuthorRepository $authorRepository, SerializerInterface $serializer, Request $request, TagAwareCacheInterface $cache): JsonResponse
+    {
+        $page = $request->get('page', 1);
+        $limit = $request->get('limit', 3);
+
+        $idCache = "getAuthorList-" . $page . "-" . $limit;
+
+        $jsonAuthorList = $cache->get($idCache, function (ItemInterface $item) use ($authorRepository, $page, $limit, $serializer) {
+            $item->tag("authorsCache");
+            $authorList = $authorRepository->findAllWithPagination($page, $limit);
+            $context = SerializationContext::create()->setGroups(["getAllAuthor"]);
+            return $serializer->serialize($authorList, 'json', $context);
+        });
+
+        return new JsonResponse($jsonAuthorList, Response::HTTP_OK, [], true);
+    }
+
+    #[Route('/api/authors/{id}', name: 'detailAuthor', methods: ['GET'])]
+    public function getDetailAuthor(Author $author, SerializerInterface $serializer): JsonResponse
+    {
+        $context = SerializationContext::create()->setGroups(["getAllAuthor"]);
+        $jsonAuthor = $serializer->serialize($author, 'json', $context);
+        return new JsonResponse($jsonAuthor, Response::HTTP_OK, ['accept' => 'json'], true);
+    }
+
+    #[Route('/api/authors/{id}', name: 'deleteAuthor', methods: ['DELETE'])]
+    #[IsGranted('ROLE_ADMIN', message: 'Vous n\'avez pas les droits suffisants pour supprimer un auteur')]
+    public function deleteAuthor(Author $author, EntityManagerInterface $em, TagAwareCacheInterface $cache): JsonResponse
+    {
+        $cache->invalidateTags(["authorsCache"]);
+        $em->remove($author);
+        $em->flush();
+
+        return new JsonResponse(null, Response::HTTP_NO_CONTENT);
+    }
+
+    #[Route('/api/authors', name: "create", methods: ['POST'])]
+
+    public function createAuthor(Request $request, SerializerInterface $serializer, EntityManagerInterface $em, UrlGeneratorInterface $urlGenerator
+        , ValidatorInterface             $validator): JsonResponse
+    {
+
+        $author = $serializer->deserialize($request->getContent(), Author::class, 'json');
+
+        // On vérifie les erreurs
+        $errors = $validator->validate($author);
+        if ($errors->count() > 0) {
+            return new JsonResponse($serializer->serialize($errors, 'json'), JsonResponse::HTTP_BAD_REQUEST, [], true);
+        }
+
+        $em->persist($author);
+        $em->flush();
+
+        $jsonBook = $serializer->serialize($author, 'json', ['groups' => 'getAuthor'], ['groups' => 'getAllAuthor']);
+
+        $location = $urlGenerator->generate('detailAuthor', ['id' => $author->getId()], UrlGeneratorInterface::ABSOLUTE_URL);
+
+        return new JsonResponse($jsonBook, Response::HTTP_CREATED, ["Location" => $location], true);
+    }
+
+    #[Route('/api/authors/{id}', name: "updateAuthor", methods: ['PUT'])]
+    #[IsGranted('ROLE_ADMIN', message: 'Vous n\'avez pas les droits suffisants pour éditer un auteur')]
+    public function updateAuthor(Request $request, SerializerInterface $serializer, Author $currentAuthor, EntityManagerInterface $em, ValidatorInterface $validator): JsonResponse
+    {
+        $newAuthor = $serializer->deserialize($request->getContent(), Book::class, 'json');
+        $currentAuthor->setTitle($newAuthor->getTitle());
+        $currentAuthor->setCoverText($newAuthor->getCoverText());
+
+        // On vérifie les erreurs
+        $errors = $validator->validate($currentAuthor);
+        if ($errors->count() > 0) {
+            return new JsonResponse($serializer->serialize($errors, 'json'), JsonResponse::HTTP_BAD_REQUEST, [], true);
+        }
+
+        $updatedAuthor = $serializer->deserialize($request->getContent(), Author::class, 'json', [AbstractNormalizer::OBJECT_TO_POPULATE => $currentAuthor]);
+
+        $em->persist($updatedAuthor);
+        $em->flush();
+        return new JsonResponse(null, JsonResponse::HTTP_NO_CONTENT);
+    }
+}
